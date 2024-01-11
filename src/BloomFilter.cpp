f1af0831156575ea8e5d67f354e3bafc24688b4b
AsifMadar <asifmadar5@gmail.com>
2024-01-11 08:53:13 +0200

    BloomFilter class

---
+++ src/BloomFilter.cpp
@@ -0,0 +1,14 @@
+class BloomFilter{
+private:
+int numOfHashesFirstFun;
+int numOfHashesSecondFun;
+int[] bitArray;
+public:
+// initializing the BloomFilter instance.
+BloomFilter(int size, int numOfHashesFirstFun, numOfHashesSecondFun) {
+    bitArray = new int[size];
+    this.numOfHashesFirstFun = numOfHashesFirstFun;
+    this.numOfHashesSecondFun = numOfHashesSecondFun;
+}
+
+};
