84a2e887814e2032e90e748723afb065246ee37b
michaelts1 <41857330+michaelts1@users.noreply.github.com>
2024-01-12 02:52:04 +0200

    userInput.cpp: add header file and use in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <vector>
+#include "./userInput.h"
 
 enum class Action { Unset, AddURL, CheckURL, };
 
 int main() {
 	Action action = Action::Unset;
+	std::vector<int>* initialSettingsVec = new std::vector<int>;
+	readLineNumbers(std::cin, initialSettingsVec, 2, 3);
+	for (int n: *initialSettingsVec) std::cout << n << ", ";
+	std::cout << '\n';
 
 	return 0;
 }
---
+++ src/userInput.h
@@ -0,0 +1,1 @@
+int readLineNumbers(std::istream& input, std::vector<int>* iVector, unsigned short minNums = 1, unsigned short maxNums = 0);
