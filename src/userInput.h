30f424adfb595f81bdf5bb54d323e01b337c150a
michaelts1 <41857330+michaelts1@users.noreply.github.com>
2024-01-12 13:00:06 +0200

    Add pragma once, includes to header file

--- src/userInput.h
+++ src/userInput.h
@@ -1,1 +1,6 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <vector>
+
 int readLineNumbers(std::istream& input, std::vector<int>* iVector, unsigned short minNums = 1, unsigned short maxNums = 0);
