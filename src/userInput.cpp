c4a9722bcb052a5fb9a0f603c07adb54ae54b09e
michaelts1 <41857330+michaelts1@users.noreply.github.com>
2024-01-12 02:46:01 +0200

    TDD: refactor userInput.cpp

--- src/userInput.cpp
+++ src/userInput.cpp
@@ -4,9 +4,13 @@
 
 /// @brief Reads lines from the provided input stream until a line that contains only numbers is found.
 /// 	Then, fills the provided vector with the numbers found on that line.
-/// @param input
-/// @param iVector A vector to store the numbers in. The vector will be cleared before reading the line.
-void readLineNumbers(std::istream& input, std::vector<int>* iVector) {
+/// 	If no valid lines were found in the input stream, the vector will be empty and the function will return -1.
+/// @param input An input stream reference
+/// @param iVector A pointer to a vector that will store the numbers. The vector will be cleared before reading the line.
+/// @param minNums The minimum amount of numbers that has to exist in the same input line to consider it valid (inclusive). Defaults to 1.
+/// @param maxNums The maximum amount of numbers that has to exist in the same input line to consider it valid (inclusive). Defaults to 0 (unrestricted).
+/// @return The function returns 0 if a valid line was found and -1 otherwise.
+int readLineNumbers(std::istream& input, std::vector<int>* iVector, unsigned short minNums = 1, unsigned short maxNums = 0) {
 	while (!input.eof()) {
 		iVector->clear();
 
@@ -14,14 +18,17 @@ void readLineNumbers(std::istream& input, std::vector<int>* iVector) {
 		std::getline(input, line);
 		std::stringstream lineStream(line);
 
-		char nextChar = lineStream.peek();
-		for (int n; nextChar != EOF && !lineStream.fail(); nextChar = lineStream.peek()) {
+		for (char nextChar = lineStream.peek(); nextChar != EOF && !lineStream.fail(); nextChar = lineStream.peek()) {
+			int n;
 			lineStream >> n;
 			iVector->push_back(n);
 		}
 
 		/* Since `lineStream` only contains one line, we know it ended when the next char is EOF. However,
 			if we failed before reaching EOF, then we know the line was invalid and continue to the next line. */
-		if (lineStream.eof()) break;
+		int vecSize = iVector->size();
+		if (lineStream.eof() && minNums <= vecSize && (vecSize <= maxNums || maxNums == 0)) return 0;
 	}
+
+	return -1;
 }
--- tests/userInput_test.cpp
+++ tests/userInput_test.cpp
@@ -4,14 +4,12 @@
 TEST(ReadLineNumbersTest, ValidInput) {
 	std::istringstream dummyIStream("200 20\n8 1 2");
 	std::vector<int>* vec = new std::vector<int>;
-	readLineNumbers(dummyIStream, vec);
 
+	readLineNumbers(dummyIStream, vec);
 	ASSERT_EQ((*vec)[0], 200);
 	ASSERT_EQ((*vec)[1], 20);
 
-	vec->clear();
 	readLineNumbers(dummyIStream, vec);
-
 	ASSERT_EQ((*vec)[0], 8);
 	ASSERT_EQ((*vec)[1], 1);
 	ASSERT_EQ((*vec)[2], 2);
@@ -20,16 +18,53 @@ TEST(ReadLineNumbersTest, ValidInput) {
 TEST(ReadLineNumbersTest, InvalidInput) {
 	std::istringstream dummyIStream("10 a 5\n4 1\nhello\n 2 but why\n230 2 1");
 	std::vector<int>* vec = new std::vector<int>;
-	readLineNumbers(dummyIStream, vec);
 
-	// Should read next line instead
+	readLineNumbers(dummyIStream, vec);
 	ASSERT_EQ((*vec)[0], 4);
 	ASSERT_EQ((*vec)[1], 1);
 
-	vec->clear();
 	readLineNumbers(dummyIStream, vec);
-
 	ASSERT_EQ((*vec)[0], 230);
 	ASSERT_EQ((*vec)[1], 2);
 	ASSERT_EQ((*vec)[2], 1);
+}
+
+TEST(ReadLineNumbersTest, MinNumbers) {
+	std::istringstream dummyIStream("1 2\n 5\n 4 3 5\n 41");
+	std::vector<int>* vec = new std::vector<int>;
+
+	readLineNumbers(dummyIStream, vec, 2);
+	ASSERT_EQ((*vec)[0], 1);
+	ASSERT_EQ((*vec)[1], 2);
+
+	readLineNumbers(dummyIStream, vec, 3);
+	ASSERT_EQ((*vec)[0], 4);
+	ASSERT_EQ((*vec)[1], 3);
+	ASSERT_EQ((*vec)[2], 5);
+
+	readLineNumbers(dummyIStream, vec);
+	ASSERT_EQ((*vec)[0], 41);
+}
+
+TEST(ReadLineNumbersTest, MaxNumbers) {
+	std::istringstream dummyIStream("1 2 3\n 5\n2\n 4 3 5\n 41 2");
+	std::vector<int>* vec = new std::vector<int>;
+
+	readLineNumbers(dummyIStream, vec, 1, 2);
+	ASSERT_EQ((*vec)[0], 5);
+
+	readLineNumbers(dummyIStream, vec, 2, 3);
+	ASSERT_EQ((*vec)[0], 4);
+	ASSERT_EQ((*vec)[1], 3);
+	ASSERT_EQ((*vec)[2], 5);
+
+	ASSERT_EQ(readLineNumbers(dummyIStream, vec, 1, 1), -1);
+}
+
+TEST(ReadLineNumbersTest, ReturnValue) {
+	std::istringstream dummyIStream("1 2\n 5\n 41");
+	std::vector<int>* vec = new std::vector<int>;
+
+	ASSERT_EQ(readLineNumbers(dummyIStream, vec, 2), 0);
+	ASSERT_EQ(readLineNumbers(dummyIStream, vec, 2), -1);
 }
