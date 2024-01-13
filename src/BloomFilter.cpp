c4c7c2df9fb79beb1eb59bc8309024d82c11c523
AsifMadar <asifmadar5@gmail.com>
2024-01-13 19:40:09 +0200

    getItemFunction

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <iostream>
 
 using namespace std;
 
@@ -13,17 +14,23 @@ vector<int> bitArray;
 hash<string> stringFirstHashFun;
 hash<string> stringSecondHashFun;
 
-    int hashItIn(int hashTimes, string url, hash<string> hashFun) {
+    int hashItIn(int hashTimes, string url, hash<string> hashFun, bool secondHashFun) {
             int value = hashFun(url); //Starting the hashing here, because we need to hash more then one time
+            if (secondHashFun) { 
+                    value *=31;
+                }
             for(int i = 0; i < hashTimes - 1; i++ ) {//We hashed once
                 value = hashFun(to_string(value)); //Now we can hash the value again and again
+                if (secondHashFun) { //Use the second hash function because we don't get different function using hash<string>
+                    value *=31;
+                }
                 }
             return value % bitArray.size();
         }
 
     public:
     // initializing the BloomFilter instance.
-    BloomFilter(int size, int numOfHashesFirstFun, int numOfHashesSecondFun) : bitArray(size),  numOfHashesFirstFun(numOfHashesFirstFun), numOfHashesSecondFun(numOfHashesSecondFun) {}
+    BloomFilter(int size, int numOfHashesFirstFun, int numOfHashesSecondFun) : bitArray(size),  numOfHashesFirstFun(numOfHashesFirstFun), numOfHashesSecondFun(numOfHashesSecondFun) { }
 
     std::vector<int> getBitArray() {
         return this->bitArray;
@@ -39,11 +46,21 @@ hash<string> stringSecondHashFun;
 
     // Adding the item to the blacklist.
     void addItem(string url) {
-        int FirstFuncUrlValue = hashItIn(numOfHashesFirstFun, url, stringFirstHashFun); //calls for a method returns the int value after hashing times as requested
-        if (numOfHashesSecondFun != 0) { //checks if we need to use the other function
-            int secondFuncUrlValue = hashItIn(numOfHashesSecondFun, url, stringSecondHashFun);
-            bitArray[secondFuncUrlValue] = 1;
-        } // initialize 1 in the dedicated place in the array.
+        int FirstFuncUrlValue = hashItIn(numOfHashesFirstFun, url, stringFirstHashFun, false); //calls for a method returns the int value after hashing times as requested
         bitArray[FirstFuncUrlValue] = 1;
+        cout << FirstFuncUrlValue << endl;
+        if (numOfHashesSecondFun != 0) { //checks if we need to use the other function
+            int secondFuncUrlValue = hashItIn(numOfHashesSecondFun, url, stringSecondHashFun, true);
+            bitArray[secondFuncUrlValue] = 1;
+                    cout << secondFuncUrlValue << endl;
+        } // initialize 1 in the dedicated place in the array.
+    }
+    bool getItem(string url) {
+        int FirstFuncUrlValue = hashItIn(numOfHashesFirstFun, url, stringFirstHashFun, false); //calls for a method returns the int value after hashing times as requested
+        if (numOfHashesSecondFun != 0) { //checks if we need to use the other function
+            int secondFuncUrlValue = hashItIn(numOfHashesSecondFun, url, stringSecondHashFun, true);
+            return bitArray[FirstFuncUrlValue] == 1 && bitArray[secondFuncUrlValue] == 1;
+    }
+        return bitArray[FirstFuncUrlValue] == 1; //If there's no second hash function
     }
 };
