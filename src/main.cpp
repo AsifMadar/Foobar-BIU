e095c7e5b5a9b0a4d43cd1005d2deff0488e8bbd
michaelts1 <41857330+michaelts1@users.noreply.github.com>
2024-01-14 05:23:12 +0200

    Create the string blacklist for validating the BloomFilter responses

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <functional>
 #include <iostream>
 #include <stdexcept>
+#include <string>
 #include <vector>
 #include "./hashFuncs.h"
 #include "./userInput.h"
@@ -12,6 +13,7 @@ void createBloomFilter();
 int main() {
 	Action action = Action::Unset;
 	createBloomFilter();
+	std::vector<std::string> blackList = {}; // Will be used to validate suspected false-positives
 
 	return 0;
 }
@@ -28,7 +30,6 @@ void createBloomFilter() {
 	for (int i = 1; i < settingsVec.size(); i++) {
 		hashFunctionsVec.push_back(hashFuncs::functions[settingsVec[i] - 1]);
 	}
-
 
 	// Once we have a header file for BloomFilter.cpp, we should change the return type and return the bloom filter
 	// return new BloomFilter(bloomBitArrayLength, hashFunctionsVec);
